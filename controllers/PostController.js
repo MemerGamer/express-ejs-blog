cc32c111f512c9c46aa37b64e3f468bf23f8db51
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-06-18 22:27:06 +0300

    check if user is logged in, based on that display navbar

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,20 +4,27 @@ import CommentModel from "../models/CommentModel.js";
 
 async function landingPage(_req, res, _next) {
   const posts = await PostModel.getAllPosts();
+  // a user is logged in if there is token in the cookies
+  const isLoggedIn = !!_req.cookies.token;
+  // console.log(_req.session);
+
   res.locals = {
     title: "Blog",
     posts: posts,
+    isLoggedIn: isLoggedIn,
   };
   res.render("index");
 }
 
 async function postBySlug(req, res, _next) {
   const { slug } = req.params;
-  console.log(slug);
+  //console.log(slug);
   const post = await PostModel.getPostBySlug(slug);
+  const isLoggedIn = !!req.cookies.token;
   res.locals = {
     title: "Blog",
     post: post.at(0),
+    isLoggedIn: isLoggedIn,
     comments: await CommentModel.getAllCommentsForPostSlug(slug),
   };
   res.render("post");
