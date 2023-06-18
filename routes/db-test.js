92a2a2bd2610f05202a3378d43aa1897329c84d1
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-06-18 19:25:12 +0300

    Delete db-test.js

--- routes/db-test.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import DBTestController from "../controllers/DBTestController.js";
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', DBTestController.testPage);
-
-export default router;
