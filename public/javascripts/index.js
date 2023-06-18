237a63d748e971eaa1bfddfc7c42acaaf5014a7e
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-06-18 19:25:22 +0300

    Update index.js

--- public/javascripts/index.js
+++
@@ -1,7 +0,0 @@
-const feelingLuckyBtn = document.querySelector("#feeling-lucky-btn");
-
-feelingLuckyBtn.addEventListener("click", () => {
-    const catGif = document.querySelector("#cat-gif");
-    catGif.style.display = "block";
-    catGif.src = `https://cataas.com/cat/gif?${new Date().getTime()}`;
-});
