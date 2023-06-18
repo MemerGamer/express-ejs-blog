ba83ea79d0d2bf61955d14fea65bae8252b7c996
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-06-18 15:57:40 +0300

    fixed user model for my database

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,18 +1,21 @@
 import logger from "../utils/logging.js";
-import db from '../db/db.js';
+import db from "../db/db.js";
 
 async function getUserById(userID) {
-    // Get a connection
-    const conn = db.getConnection();
+  // Get a connection
+  const conn = db.getConnection();
 
-    // Make a query
-    const [result, _columnDefinition] = await conn.query('SELECT * FROM users WHERE id = ?', [userID]);
+  // Make a query
+  const [result, _columnDefinition] = await conn.query(
+    "SELECT * FROM Users WHERE id = ?",
+    [userID]
+  );
 
-    logger.debug(`User with id(${userID}), %o`, result[0]);
+  logger.debug(`User with id(${userID}), %o`, result[0]);
 
-    return result;
+  return result;
 }
 
 export default {
-    getUserById
-}
+  getUserById,
+};
